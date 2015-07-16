32e6a236b70d437487c6e9788bbe99d00eee1291
aeremin <aeremin@maxus.ru>
2015-07-16 10:02:28 +0300

    mmkay

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -27,4 +27,4 @@ def shortened_tweet_truncator(word)
   word.length > 140 ? "#{word[0..135]} ..." : word
 end
 
-puts word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
+#puts word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
